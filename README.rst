56d6fd40a2cfdf925dd3a88f82fa57dc5ac1a95e
Matt McCormick <matt.mccormick@kitware.com>
2017-11-06 15:28:19 -0500

    ENH: Initial commit
    Output of the ITKModuleTemplate cookiecutter.

---
+++ README.rst
@@ -0,0 +1,15 @@
+ITKVtkGlue
+=================================
+
+.. image:: https://circleci.com/gh/InsightSoftwareConsortium/ITKVtkGlue.svg?style=shield
+    :target: https://circleci.com/gh/InsightSoftwareConsortium/ITKVtkGlue
+
+.. image:: https://travis-ci.org/InsightSoftwareConsortium/ITKVtkGlue.svg?branch=master
+    :target: https://travis-ci.org/InsightSoftwareConsortium/ITKVtkGlue
+
+.. image:: https://img.shields.io/appveyor/ci/itkrobot/itkvtkglue.svg
+    :target: https://ci.appveyor.com/project/itkrobot/itkvtkglue
+
+This module contains classes that bridge and ITK and VTK image processing and visualization pipeline.
+
+ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. VTK enables visualization of images in three dimension. This module converts ITK and VTK image data structures in a processing pipelining without copying the pixel buffer.
---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from os import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('scikit-build is required to build from source.', file=sys.stderr)
+    print('Please run:', file=sys.stderr)
+    print('', file=sys.stderr)
+    print('  python -m pip install scikit-build')
+    sys.exit(1)
+
+setup(
+    name='itk-vtkglue',
+    version='0.1.0',
+    author='Insight Software Consortium',
+    author_email='itk+community@discourse.itk.org',
+    packages=['itk'],
+    package_dir={'itk': 'itk'},
+    download_url=r'https://github.com/Kitware/ITKVtkGlue',
+    description=r'This module contains classes that bridge and ITK and VTK image processing and visualization pipeline.',
+    long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. VTK enables visualization of images in three dimension. This module converts ITK and VTK image data structures in a processing pipelining without copying the pixel buffer.',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: Android",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+    license='Apache',
+    keywords='ITK InsightToolkit',
+    url=r'https://itk.org/',
+    install_requires=[
+        r'itk'
+    ]
+    )
