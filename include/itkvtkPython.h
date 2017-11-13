ae00c6d205457b680cb4da6827b6936a28614a00
Matt McCormick <matt.mccormick@kitware.com>
2017-11-13 15:42:39 -0500

    BUG: Add license header to itkvtkPython.h

--- include/itkvtkPython.h
+++ include/itkvtkPython.h
@@ -1,4 +1,20 @@
-
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #if PY_VERSION_HEX < 0x3020000
 #ifdef Py_hash_t
 #undef Py_hash_t
