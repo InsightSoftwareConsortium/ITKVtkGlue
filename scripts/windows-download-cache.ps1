4b412e2d20246c72d2125d9b8bc7d8b414b5d5d5
Matt McCormick <matt.mccormick@kitware.com>
2018-10-25 14:18:12 -0400

    ENH: Add windows-download-cache.ps1

---
+++ scripts/windows-download-cache.ps1
@@ -0,0 +1,17 @@
+trap { Write-Error $_; Exit 1 }
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
+if (-not (Test-Path env:APPVEYOR)) { iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/scikit-build/scikit-ci-addons/master/windows/install-python.ps1')) }
+if (-not (Test-Path env:ITK_PACKAGE_VERSION)) { $env:ITK_PACKAGE_VERSION = 'v5.0b01' }
+Invoke-WebRequest -Uri "https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/$env:ITK_PACKAGE_VERSION/ITKPythonBuilds-windows.zip" -OutFile "ITKPythonBuilds-windows.zip"
+sz x ITKPythonBuilds-windows.zip -oC:\P -aoa -r
+if (-not (Test-Path env:VTK_PACKAGE_VERSION)) { $env:VTK_PACKAGE_VERSION = 'v8.1.1' }
+Invoke-WebRequest -Uri "https://github.com/KitwareMedical/VTKPythonBuilds/releases/download/$env:VTK_PACKAGE_VERSION/VTKPythonBuilds-windows.zip" -OutFile "VTKPythonBuilds-windows.zip"
+sz x VTKPythonBuilds-windows.zip -oC:\P -aoa -r
+Invoke-WebRequest -Uri "http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.11.windows.bin.zip" -OutFile "doxygen-1.8.11.windows.bin.zip"
+sz x doxygen-1.8.11.windows.bin.zip -oC:\P\doxygen -aoa -r
+Invoke-WebRequest -Uri "https://data.kitware.com/api/v1/file/5bbf87ba8d777f06b91f27d6/download/grep-win.zip" -OutFile "grep-win.zip"
+sz x grep-win.zip -oC:\P\grep -aoa -r
+$env:Path += ";C:\P\grep"
